a7a46aa670e37ca54865285c5930e25dacf39b79
dinesh <dinesh.bharti.be16@gmail.com>
2023-10-05 21:09:36 -0700

    python3 constructor call

--- python3.py
+++ python3.py
@@ -1,5 +1,5 @@
 # Constructor
-
+'''
 class Employee:
     eid=0
     def getdetails(self):
@@ -25,3 +25,34 @@ rakes.getdetails()
 # Id is : 2
 # Name is : skkkk
 # Monthaly salary is: 23000
+
+'''
+class Employee():
+    def __init__(self):
+        print("Constructor called.")
+    def getdetails(self):
+        print("ID:",self.eid)
+        print("NAME:",self.ename)
+        print("SALARY:",self.esal)
+
+vijay=Employee()
+vijay.eid=1
+vijay.ename="sanjeet"
+vijay.esal=30000
+vijay.getdetails()
+
+
+john=Employee()
+john.eid=2
+john.ename="jeet"
+john.esal=40000
+john.getdetails()
+
+# ID: 1
+# NAME: sanjeet
+# SALARY: 30000
+# Constructor called.
+# ID: 2
+# NAME: jeet
+# SALARY: 40000
+
